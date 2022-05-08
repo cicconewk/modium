c1f08274263c2f27e4e25f8ec812f5b0396ade27
Willker Romero <cicconewk@gmail.com>
2022-05-08 03:29:31 -0400

    feat: initial build w/header component

---
+++ components/Header.tsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import Link from 'next/link'
+
+import ModiumLogo from '../assets/modium-logo.webp'
+import Image from 'next/image'
+
+function Header() {
+  return (
+    <header className="mx-auto flex max-w-7xl justify-between p-5">
+      <div className="flex items-center space-x-5">
+        <Link href="/">
+          <img
+            className="w-44 cursor-pointer object-contain"
+            src={ModiumLogo}
+            alt="modium-logo"
+          />
+        </Link>
+        <div className="hidden items-center space-x-5 md:inline-flex">
+          <h3>About</h3>
+          <h3>Contact</h3>
+          <h3 className="rounded-full bg-green-600 px-4 py-1 text-white">
+            Follow
+          </h3>
+        </div>
+      </div>
+      <div className="flex items-center space-x-5 text-green-600">
+        <h3>Sign In</h3>
+        <h3 className="rounded-full border border-green-600 px-4 py-1">
+          Get Started
+        </h3>
+      </div>
+    </header>
+  )
+}
+
+export default Header
